a03f233a5f8e4e6c6bc89b4842f896d12100b661
BilalZonjy <bxz142@case.edu>
2018-01-08 21:07:41 -0500

    city table is working
    Signed-off-by: BilalZonjy <bxz142@case.edu>

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var Portfolio = require('../models/portfolio');
+var Profile = require('../models/Profile');
 var mid = require('../middleware');
 
 // GET /profile
@@ -18,42 +19,48 @@ router.get('/profile', mid.requiresLogin, function(req, res, next) {
 
 // GET /logout
 router.get('/logout', function(req, res, next) {
-  if (req.session) {
-    // delete session object
-    req.session.destroy(function(err) {
-      if(err) {
-        return next(err);
-      } else {
-        return res.redirect('/');
-      }
-    });
-  }
+    if (req.session) {
+        // delete session object
+        req.session.destroy(function(err) {
+            if (err) {
+                return next(err);
+            } else {
+                return res.redirect('/');
+            }
+        });
+    }
 });
 
 // GET /login
 router.get('/login', mid.loggedOut, function(req, res, next) {
-  return res.render('login', { title: 'Log In'});
+    return res.render('login', { title: 'Log In' });
 });
 
 // POST /login
 router.post('/login', function(req, res, next) {
-  if (req.body.email && req.body.password) {
-    User.authenticate(req.body.email, req.body.password, function (error, user) {
-      if (error || !user) {
-        var err = new Error('Wrong email or password.');
+    if (req.body.email && req.body.password) {
+        User.authenticate(req.body.email, req.body.password, function(error, user) {
+            if (error || !user) {
+                var err = new Error('Wrong email or password.');
+                err.status = 401;
+                return next(err);
+            } else {
+                req.session.userId = user._id;
+                return res.redirect('/profile');
+            }
+        });
+    } else {
+        var err = new Error('Email and password are required.');
         err.status = 401;
         return next(err);
-      }  else {
-        req.session.userId = user._id;
-        return res.redirect('/profile');
-      }
-    });
-  } else {
-    var err = new Error('Email and password are required.');
-    err.status = 401;
-    return next(err);
-  }
+    }
 });
+
+
+
+
+
+
 router.get('/register', mid.loggedOut, function(req, res, next) {
     return res.render('register', { title: 'Sign Up' });
 });
@@ -93,8 +100,6 @@ router.post('/register', function(req, res, next) {
                 };
                 Portfolio.create(userportfolio, function(error, user) {
                     if (error) { cosole.log(error) }
-
-
                 });
                 return res.redirect('/profile');
             }
@@ -112,34 +117,49 @@ router.post('/register', function(req, res, next) {
 
 // GET /
 router.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
+    return res.render('index', { title: 'Home' });
 });
 
 // GET /about
 router.get('/about', function(req, res, next) {
-  return res.render('about', { title: 'About' });
+    return res.render('about', { title: 'About' });
 });
-router.get('/facts', function (req, res, next) {
-  return res.render('facts', { title: 'CryptoKnow' });
+router.get('/facts', function(req, res, next) {
+    return res.render('facts', { title: 'CryptoKnow' });
 });
 // router.get('/dashboard', function (req, res, next) {
 //   return res.render('dashboard', { title: 'Dashboard' });
 // });
 router.get('/dashboard', mid.requiresLogin, function(req, res, next) {
-  User.findById(req.session.userId)
-      .exec(function (error, user) {
-        if (error) {
-          return next(error);
-        } else {
-          return res.render('dashboard', { title: 'dashboard', name: user.name });
-        }
-      });
+    User.findById(req.session.userId)
+        .exec(function(error, user) {
+            if (error) {
+                return next(error);
+            } else {
+                return res.render('dashboard', { title: 'dashboard', name: user.name });
+            }
+        });
 });
 
 
+router.post('/updateProfile', mid.requiresLogin, function(req, res, next) {
+
+    var profileData = {
+        id: req.session.userId,
+        city: req.body.city,
+        website: req.body.website,
+    };
+    console.log(profileData);
+    Profile.update({ _id: req.session.userId }, profileData, { upsert: true }, function(error, user) {
+        if (error) { console.log(error) }
+    });
+    return res.redirect('/profile');
+
+});
+
 // GET /contact
 router.get('/contact', function(req, res, next) {
-  return res.render('contact', { title: 'Contact' });
+    return res.render('contact', { title: 'Contact' });
 });
 
 
