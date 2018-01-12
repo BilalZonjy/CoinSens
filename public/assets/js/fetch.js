ad408f98c382eb10bf2b1e74892753e927503673
BilalZonjy <bxz142@case.edu>
2018-01-12 17:10:59 -0500

    dashboard is working
    Signed-off-by: BilalZonjy <bxz142@case.edu>

--- public/assets/js/fetch.js
+++ public/assets/js/fetch.js
@@ -1,70 +1,59 @@
-console.log('test')
+function get_ticker_price_and_change_color(url, divID) {
+    // var btc_url_change = 'https://api.cryptonator.com/api/ticker/btc-usd';
+    $.get(url, function(data) {
+        var change = Math.round(data.ticker.change * 100) / 100;
+        $('#' + divID).text(Math.abs(change));
+        if (change < 0) {
+            $('#' + divID).css('color', '#b83942');
+        } else {
+            $('#' + divID).css('color', '#4aaaa5');
+        }
+    });
 
-// function createNode(element) {
-//     return document.createElement(element);
-// }
 
-// function append(parent, el) {
-//     return parent.appendChild(el);
-// }
 
-// const ul = document.getElementById('btc');
+}
+
+
+
+
+
 const url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH,BTC,LTC&tsyms=BTC,USD,EUR';
 
-
-$.get(url, function(data) {
-    $('#currentBTC').html(data.BTC.USD);
-    var btcAmount = $('#currentBTC').data('cry').btc * data.BTC.USD;
-    var btcText = $('#calcBTC').text();
-    $('#calcBTC').text(btcText + Math.round(btcAmount * 100) / 100 + ' USD');
-    console.log(btcAmount);
-    $('#currentBTC').addClass('counter');
-    $('#currentETH').html(data.ETH.USD);
-    var ethAmount = $('#currentETH').data('cry').eth * data.ETH.USD;
-    var ethText = $('#calcETH').text();
-    $('#calcETH').text(ethText + Math.round(ethAmount * 100) / 100 + ' USD');
-    $('#currentETH').addClass('counter');
-    $('#currentLIT').html(data.LTC.USD);
-    var litAmount = $('#currentLIT').data('cry').lit * data.LTC.USD;
-    var litText = $('#calcLIT').text();
-    $('#calcLIT').text(litText + Math.round(litAmount * 100) / 100 + ' USD');
-
-    $('#currentLIT').addClass('counter');
-
-});
-
-// var today = moment();
-// var end = today.format('YYYY-MM-DD');
-// var start = today.subtract(1, 'months').format('YYYY-MM-DD');
-// var url_BTC_price = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=' + start + '&end=' + end;
-// // 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-10&end=2018-01-10'
-
-// console.log(url_BTC_price);
-
-// fetch(url_BTC_price)
-//     .then((resp) => resp.json()) // Transform the data into json
-//     .then(function(data) {
-//         console.log(data.bpi);
-//     })
+function get_prices() {
 
 
 
+    $.get(url, function(data) {
+        $('#currentBTC').removeClass('counter');
+        $('#currentETH').removeClass('counter');
+        $('#currentLIT').removeClass('counter');
+        $('#currentBTC').html(data.BTC.USD);
+        var btcAmount = $('#currentBTC').data('cry').btc * data.BTC.USD;
+        var btcText = $('#calcBTC').text().slice(0, 7);
+        $('#calcBTC').text(btcText + Math.round(btcAmount * 100) / 100 + ' USD');
+        $('#currentBTC').addClass('counter');
+        $('#currentETH').html(data.ETH.USD);
+        var ethAmount = $('#currentETH').data('cry').eth * data.ETH.USD;
+        var ethText = $('#calcETH').text();
+        $('#calcETH').text(ethText + Math.round(ethAmount * 100) / 100 + ' USD');
+        $('#currentETH').addClass('counter');
+        $('#currentLIT').html(data.LTC.USD);
+        var litAmount = $('#currentLIT').data('cry').lit * data.LTC.USD;
+        var litText = $('#calcLIT').text();
+        $('#calcLIT').text(litText + Math.round(litAmount * 100) / 100 + ' USD');
 
+        $('#currentLIT').addClass('counter');
 
+    });
+    get_ticker_price_and_change_color('https://api.cryptonator.com/api/ticker/btc-usd', 'btccoin');
+    get_ticker_price_and_change_color('https://api.cryptonator.com/api/ticker/eth-usd', 'ethcoin');
+    get_ticker_price_and_change_color('https://api.cryptonator.com/api/ticker/ltc-usd', 'litcoin');
+    get_ticker_price_and_change_color('https://api.cryptonator.com/api/ticker/xrp-usd', 'ripplecoin');
+    get_ticker_price_and_change_color('https://api.cryptonator.com/api/ticker/dash-usd', 'monerocoin');
+    get_ticker_price_and_change_color('https://api.cryptonator.com/api/ticker/XMR-usd', 'dashcoin');
+    get_ticker_price_and_change_color('https://api.cryptonator.com/api/ticker/doge-usd', 'dogecoin');
+}
 
-
-
-// fetch(url)
-//     .then((resp) => resp.json())
-//     .then(function(data) {
-//         let btc = data.results;
-//         console.log(btc);
-//         // return btc.map(function (btc) {
-//         //     let span = createNode('span');
-//         //     span.innerHTML = `${btc.BTC.USD}`;
-//         //     append(p);
-//         // })
-//     })
-//     .catch(function(error) {
-//         console.log(error);
-//     });
+get_prices();
+setInterval(get_prices, 30000);
